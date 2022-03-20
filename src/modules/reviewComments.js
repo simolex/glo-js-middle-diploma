5a236d9948ecb2e7301a1c00fb706c4700824b40
simolex <simolex@yandex.ru>
2022-03-20 23:41:09 +0300

    Выполнено дополнительное задание по отображению комментариев

--- src/modules/reviewComments.js
+++ src/modules/reviewComments.js
@@ -1,38 +1,64 @@
 export const reviewComments = () => {
   const reviewContainer = document.querySelector(".comments-container");
+  let currentComment = 0;
+  let lastAppendedComment = {};
+  console.dir(reviewContainer);
 
-  const createComment = ({ nameWho, avatarPath, commentText }) => {
+  const createComment = ({ nameWho, avatarPath, commentText, index }) => {
     const comment = document.createElement("div");
     comment.classList.add("comment-item");
     comment.classList.add("row");
-    return `<div class="col-xs-3 col-sm-2">
-                <div class="review-user">
-                  <img src="${avatarPath ? avatarPath : "images/avatar.svg"}" alt="" class="img-responsive avatar" />
-                </div>
-              </div>
-              <div class="col-xs-9 col-sm-9">
-                <div class="review-inner review-green review-arrow review-arrow-left">
-                  <p class="text-normal">${nameWho ? nameWho : ""}</p>
-                  <p>
-                    ${
-                      commentText
-                        ? commentText
-                        : `
-  <div class="sk-three-bounce">
-    <div class="sk-bounce-1 sk-child"></div>
-    <div class="sk-bounce-2 sk-child"></div>
-    <div class="sk-bounce-3 sk-child"></div>
-  </div>
-  `
-                    }
-                    
-                  </p>
-                </div>
-              </div>`;
+    const colorIndex = +index % 3;
+    const color = colorIndex < 2 ? (colorIndex === 0 ? "review-green" : "review-gray") : "review-orange";
+    const avatarBlock = `
+      <div class="col-xs-3 col-sm-2">
+        <div class="review-user">
+          <img src="${avatarPath}" alt="" class="img-responsive avatar" />
+        </div>
+      </div>
+    `;
+    const commentBlock = `
+      <div class="col-xs-9 col-sm-9">
+        <div class="review-inner ${color} review-arrow ${+index % 2 == 1 ? "review-arrow-left" : "review-arrow-right"}">
+          <p class="text-normal">${nameWho ? nameWho : ""}</p>
+          <p>
+            ${
+              commentText
+                ? commentText
+                : `
+      <div class="sk-three-bounce">
+        <div class="sk-bounce-1 sk-child"></div>
+        <div class="sk-bounce-2 sk-child"></div>
+        <div class="sk-bounce-3 sk-child"></div>
+      </div>
+      `
+            }
+            
+          </p>
+        </div>
+      </div>
+      `;
+    comment.innerHTML = +index % 2 == 1 ? avatarBlock + commentBlock : commentBlock + avatarBlock;
+    reviewContainer.append(comment);
+    return comment;
   };
 
-  reviewContainer.innerHTML = createComment({});
-  console.log(reviewContainer);
+  const createItem = (data) => {
+    const dbIndex = currentComment % data.comments.length;
+    const createdComment = createComment({
+      nameWho: data.comments[dbIndex].author,
+      avatarPath:
+        data.comments[dbIndex].image !== "" ? `images/users/${data.comments[dbIndex].image}` : "images/avatar.svg",
+      commentText: data.comments[dbIndex].comment,
+      index: currentComment + 1,
+    });
+    currentComment++;
+    return createdComment;
+  };
+
+  reviewContainer.innerHTML = "";
+  createComment({});
+
   fetch("comments.json")
     .then((resp) => {
       console.log(resp);
@@ -43,8 +69,16 @@ export const reviewComments = () => {
       }
     })
     .then((data) => {
-      //reviewContainer.innerHTML = "reviewContainer.innerHTML";
-      console.log(data);
+      reviewContainer.innerHTML = "";
+      for (let i = 0; i < 3; i++) {
+        lastAppendedComment = createItem(data);
+      }
+
+      setInterval(() => {
+        reviewContainer.querySelectorAll(".comment-item")[0].remove();
+        lastAppendedComment.classList.add("review-margin-bottom");
+        lastAppendedComment = createItem(data);
+      }, 2000);
     })
     .catch((e) => {
       console.log(e.message);
